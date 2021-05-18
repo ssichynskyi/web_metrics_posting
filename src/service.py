b4d37f08fcfc491522bf3e1f18dbcb9b26f10c09
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-19 00:30:46 +0200

    restructure service to enable testability

--- src/service.py
+++ src/service.py
@@ -7,6 +7,33 @@ from src.consumer import Consumer
 from utils.env_config import config
 
 
+TOPIC = 'website-metrics'
+
+
+_sleep_after_request = config['Metrics endpoint']['Aiven']['upload every']
+_kafka_url = config['Metrics endpoint']['Aiven']['Kafka']['host']
+_kafka_port = str(config['Metrics endpoint']['Aiven']['Kafka']['port'])
+_kafka_uri = ':'.join((_kafka_url, _kafka_port))
+_ca_path = os.environ['CA-CERT']
+_cert_path = os.environ['SERVICE_CERT']
+_key_path = os.environ['SERVICE-KEY']
+
+aiven_kafka_consumer = Consumer(
+    TOPIC,
+    service_uri=_kafka_uri,
+    ca_path=_ca_path,
+    cert_path=_cert_path,
+    key_path=_key_path
+)
+db = WebMonitoringDBWrapper(
+    'website_metrics',
+    os.environ['DB_LOGIN'],
+    os.environ['DB_PASS'],
+    config['Metrics endpoint']['Aiven']['Postgres']['host'],
+    config['Metrics endpoint']['Aiven']['Postgres']['port']
+)
+
+
 def main(consumer: Consumer, db_wrapper: WebMonitoringDBWrapper, sleep_time: int):
     """Service runner for fetching data from Kafka broker and posting to DB
 
@@ -31,30 +58,9 @@ def main(consumer: Consumer, db_wrapper: WebMonitoringDBWrapper, sleep_time: int
 
 
 if __name__ == '__main__':
-    sleep_after_request = config['Metrics endpoint']['Aiven']['upload every']
-    _kafka_url = config['Metrics endpoint']['Aiven']['Kafka']['host']
-    _kafka_port = str(config['Metrics endpoint']['Aiven']['Kafka']['port'])
-    kafka_uri = ':'.join((_kafka_url, _kafka_port))
-    ca_path = os.environ['CA-CERT']
-    cert_path = os.environ['SERVICE_CERT']
-    key_path = os.environ['SERVICE-KEY']
-    # ToDo: pass topic as a sys arg
-    aiven_kafka_consumer = Consumer(
-        'website-metrics',
-        service_uri=kafka_uri,
-        ca_path=ca_path,
-        cert_path=cert_path,
-        key_path=key_path
-    )
-    db = WebMonitoringDBWrapper(
-        'website_metrics',
-        os.environ['DB_LOGIN'],
-        os.environ['DB_PASS'],
-        config['Metrics endpoint']['Aiven']['Postgres']['host'],
-        config['Metrics endpoint']['Aiven']['Postgres']['port']
-    )
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s | %(name)s >>> %(message)s',
         datefmt='%d-%b-%Y %H:%M:%S'
     )
-    main(aiven_kafka_consumer, db, sleep_after_request)
+    # ToDo: pass topic as a sys arg
+    main(aiven_kafka_consumer, db, _sleep_after_request)
