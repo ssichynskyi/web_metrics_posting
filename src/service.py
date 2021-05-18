262f5c8d919044d6e6966d59f070db48464cd5c8
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-05-18 02:30:17 +0200

    add kafka consumer and db pusher service

---
+++ src/service.py
@@ -0,0 +1,58 @@
+import logging
+import os
+import time
+
+from src.postgres_wrapper import WebMonitoringDBWrapper
+from src.consumer import Consumer
+from utils.env_config import config
+
+
+log = logging.getLogger(__name__)
+
+
+def main(consumer: Consumer, db_wrapper: WebMonitoringDBWrapper, sleep_time: int):
+    """Service runner for fetching data from Kafka broker and posting to DB
+
+    Args:
+        consumer: Kafka consumer
+        db_wrapper: helper lib to work with DB
+        sleep_time: number of seconds to wait between metric collection
+
+    Returns:
+        None, runs until interrupted by user
+
+    """
+    while True:
+        with consumer:
+            data = consumer.fetch_latest()
+            if not data:
+                log.warning('No data to push to DB. Is web metric service running?')
+            else:
+                db_wrapper.insert(data)
+        time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    sleep_after_request = config['Metrics endpoint']['Aiven']['upload every']
+    _kafka_url = config['Metrics endpoint']['Aiven']['Kafka']['host']
+    _kafka_port = str(config['Metrics endpoint']['Aiven']['Kafka']['port'])
+    kafka_uri = ':'.join((_kafka_url, _kafka_port))
+    ca_path = os.environ['CA-CERT']
+    cert_path = os.environ['SERVICE_CERT']
+    key_path = os.environ['SERVICE-KEY']
+    # ToDo: pass topic as a sys arg
+    aiven_kafka_consumer = Consumer(
+        'website-metrics',
+        service_uri=kafka_uri,
+        ca_path=ca_path,
+        cert_path=cert_path,
+        key_path=key_path
+    )
+    db = WebMonitoringDBWrapper(
+        'website_metrics',
+        os.environ['DB_LOGIN'],
+        os.environ['DB_PASS'],
+        config['Metrics endpoint']['Aiven']['Postgres']['host'],
+        config['Metrics endpoint']['Aiven']['Postgres']['port']
+    )
+    main(aiven_kafka_consumer, db, sleep_after_request)
