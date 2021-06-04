d20ee88ca2b156edfb4dd2ed2137288c6effc085
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-06-04 23:51:43 +0200

    improve config/environment robustness

--- utils/env_config.py
+++ utils/env_config.py
@@ -10,13 +10,27 @@ from utils.dict_merger import merge_dicts
 class ConfigParser:
     """Extracts the data from the configuration file given."""
     def __new__(cls, path):
+        if not Path.exists(path):
+            return None
         with open(path, 'r') as f:
             contents = f.read()
             return yaml.safe_load(contents)
 
 
-_path_to_config = Path(os.environ['PUBLISH_CONFIG'])
-_path_to_local_config = Path(os.environ['PUBLISH_CONFIG_LOCAL'])
+_project_root = Path(os.getenv('PROJECT_ROOT', Path(__file__).parent.parent))
+_path_to_config_folder = _project_root.joinpath('config/')
+_path_to_config = Path(
+    os.getenv(
+        'PUBLISH_CONFIG',
+        _path_to_config_folder.joinpath('service.yaml')
+    )
+)
+_path_to_local_config = Path(
+    os.getenv(
+        'PUBLISH_CONFIG_LOCAL',
+        _path_to_config_folder.joinpath('service_local.yaml')
+    )
+)
 config = ConfigParser(_path_to_config)
 _config_local = ConfigParser(_path_to_local_config)
 config = merge_dicts(config, _config_local)
